63fe6e4fecd6c053ddc32b3c7634d1778657e6ea
oligertimothee <fraxy.gfx@gmail.com>
2017-12-03 16:33:55 +0100

    debug

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var mcp = require('./mcp3008.js');
 var mymcp = new mcp(18,24,4,25);
 
 for(var i = 0; i < 8; i++){
-	mymcp.read(i,(id, value) =>{
+	mymcp.read(i,(id, value) => {
 		console.log("pin #" + id + " = " + value + "/1024");
 	});
 }

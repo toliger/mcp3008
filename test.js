d0fccdc0afe8e4e50cafc88720d0d57435593ad5
oligertimothee <fraxy.gfx@gmail.com>
2017-12-03 17:47:13 +0100

    debug

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var mcp = require('./simple-mcp3008.js');
 var mymcp = new mcp(18,24,4,25, 3.3);
 
-mymcp.setresistance(0, 1000);
+mymcp.setresistance(0, 25);
 mymcp.getsensorresistance(0, (pin, value) => {
 	console.log("resistance = " + value);
 });
